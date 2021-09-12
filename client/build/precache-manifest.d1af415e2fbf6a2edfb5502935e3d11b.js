a168666982518976a945a81fb638fd6846ea016c
Edwin Nnorom <nnoromedwin@gmail.com>
2021-09-11 23:02:19 -0500

    Added changes to build

---
+++ client/build/precache-manifest.d1af415e2fbf6a2edfb5502935e3d11b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed82389e06c5929b8d68ef633b2edb7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31c32ea162fce30ba4d6",
+    "url": "/static/css/main.c64c8587.chunk.css"
+  },
+  {
+    "revision": "69bc2cd5ba736c132877",
+    "url": "/static/js/2.5299263f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.5299263f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31c32ea162fce30ba4d6",
+    "url": "/static/js/main.83bf995d.chunk.js"
+  },
+  {
+    "revision": "00fc4afc96365658fc09",
+    "url": "/static/js/runtime-main.eda899bb.js"
+  }
+]);
