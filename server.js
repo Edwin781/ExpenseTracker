b6295287ba3c616218b4d2bb089807e3199c3d80
Edwin Nnorom <nnoromedwin@gmail.com>
2021-09-12 00:06:57 -0500

    Update React Build

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ if(process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 } 
 
+console.log(path.resolve(__dirname, 'client', 'build', 'index.html'));
+
 
 const PORT = process.env.PORT || 5000;
 
