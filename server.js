1d12403c9f5d8b680d975f64b96abb60861a4c44
Edwin Nnorom <nnoromedwin@gmail.com>
2021-09-12 00:36:31 -0500

    Update React Build

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if(process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use('/', transactions)
+//app.use('/', transactions)
 
 app.use('/api/v1/transactions', transactions);
 
